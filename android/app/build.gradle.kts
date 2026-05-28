b12864f610302e988d250c6d9f01bc2401ffe9bc
PuffRice <drytwerz69@gmail.com>
2026-05-29 01:39:21 +0600

    Chnaged isar cache and gradle settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.financetracker"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.ortho.financetracker"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.financetracker"
+        applicationId = "com.ortho.financetracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
